f810fa6741271fd00e55583d31afcbe071be41e0
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-06 11:36:32 -0300

    some hover styling

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,7 +3,7 @@ import styles from './Form.module.css';
 const Form = () => {
   return (
     <form className={styles.form__container}>
-      <label>E-mail</label>
+      <label>Email address</label>
       <input type='text' placeholder='email@company.com' />
       <input type='submit' value='Subscribe to monthly newsletter' />
     </form>
