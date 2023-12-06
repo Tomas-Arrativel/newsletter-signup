8a8f24d882c8e0c4c0f216a524d4af8e25e51341
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-06 11:36:51 -0300

    finished the design of the desktop model

--- src/containers/ImgContainer/ImgContainer.tsx
+++ src/containers/ImgContainer/ImgContainer.tsx
@@ -1,7 +1,7 @@
 import styles from './ImgContainer.module.css';
 
 const ImgContainer = () => {
-  return <div className={styles.image}>ImgContainer</div>;
+  return <div className={styles.image}></div>;
 };
 
 export default ImgContainer;
