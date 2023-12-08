bfbc7841ce2f67c7a12e6da994bbe59bb8aab650
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-08 10:05:30 -0300

    doing the basics of hook-form and adding error msg

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,32 @@
 import styles from './Form.module.css';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
-    <form className={styles.form__container}>
-      <label>Email address</label>
-      <input type='text' placeholder='email@company.com' />
+    <form
+      className={styles.form__container}
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <label>
+        Email address{' '}
+        {errors.email?.type === 'required' ||
+          (errors.email?.type === 'pattern' && (
+            <p className={styles.error}>Email is required</p>
+          ))}
+      </label>
+      <input
+        type='text'
+        {...register('email', {
+          required: true,
+          pattern: /\S+@\S+\.\S+/,
+        })}
+        placeholder='email@company.com'
+      />
       <input type='submit' value='Subscribe to monthly newsletter' />
     </form>
   );
