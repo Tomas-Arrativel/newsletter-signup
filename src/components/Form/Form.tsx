f6f8a90d7c726fbc37df0b07ad28a5ca687d85cc
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-11 11:44:27 -0300

    deleting unused useState

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import styles from './Form.module.css';
 import { useForm } from 'react-hook-form';
 
